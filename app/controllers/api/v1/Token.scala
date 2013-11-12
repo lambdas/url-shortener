b05b5e3476cc0bcce1cf4c985090e01ef25e1e78
Pavel Logvinov <lambdas@me.com>
2013-11-12 19:11:24 +0700

    GET /token test.

---
+++ app/controllers/api/v1/Token.scala
@@ -0,0 +1,12 @@
+package controllers.api.v1
+
+import play.api._
+import play.api.mvc._
+
+object Token extends Controller {
+
+  def create = Action {
+    BadRequest("No, no..")
+  }
+
+}
--- test/controllers/api/v1/TokenSpec.scala
+++ test/controllers/api/v1/TokenSpec.scala
@@ -1,11 +1,14 @@
 package controllers.api.v1
 
 import test.AppSpec
+import test.ControllerUtils._
+import play.api.test.Helpers._
 
 class TokenSpec extends AppSpec {
 
-  "test smth" should "qwe" in {
-    
+  "GET /api/v1/token" should "return 403" in {
+    val result = get("/api/v1/token")
+    result.status should equal (BAD_REQUEST)
   }
 
 }
